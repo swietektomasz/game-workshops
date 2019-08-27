1594f30f3101ff41541b7ee052f78f5d9f053a5f
swietektomasz <swietek.tomasz@protonmail.com>
2019-08-27 22:14:34 +0200

    initial commit

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,24 +1,24 @@
-import {getPost} from 'api';
+import { getPost } from 'api'
 
 async function HelloWorld() {
-    const div = document.createElement('div');
-    const h1 = document.createElement('h1');
-    const h1Text = document.createTextNode('Hello Webpack-Babel-Boilerplate!');
-    
-    div.className = 'main';
-    h1.appendChild(h1Text);
-    document.body.appendChild(div);
-    div.appendChild(h1);
+  const div = document.createElement('div')
+  const h1 = document.createElement('h1')
+  const h1Text = document.createTextNode('Hello Webpack-Babel-Boilerplates!')
 
-    const postId = 1;
-    const post = await getPost(postId);
+  div.className = 'main'
+  h1.appendChild(h1Text)
+  document.body.appendChild(div)
+  div.appendChild(h1)
 
-    const postTitle = post.title || 'Oops title was null!';
-    const p = document.createElement('p');
-    const pText = document.createTextNode(postTitle);
+  const postId = 1
+  const post = await getPost(postId)
 
-    p.appendChild(pText);
-    div.appendChild(p);
+  const postTitle = post.title || 'Oops title was null!'
+  const p = document.createElement('p')
+  const pText = document.createTextNode(postTitle)
+
+  p.appendChild(pText)
+  div.appendChild(p)
 }
 
-export default HelloWorld;
+export default HelloWorld
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-require("@babel/register");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+require('@babel/register')
 
 const config = {
-  entry: ['@babel/polyfill','./src/index.js'],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: __dirname + '/public',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules : [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        hash: true
-    })
+      hash: true,
+    }),
   ],
   resolve: {
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ]
+    modules: [path.resolve('./src'), path.resolve('./node_modules')],
   },
   devServer: {
     contentBase: __dirname + '/public',
@@ -38,20 +39,20 @@ const config = {
     port: 9000,
     open: true,
     stats: {
-        assets: false,
-        children: false,
-        chunks: false,
-        chunkModules: false,
-        colors: true,
-        entrypoints: false,
-        hash: false,
-        modules: false,
-        timings: false,
-        version: false,
-    }
+      assets: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+      colors: true,
+      entrypoints: false,
+      hash: false,
+      modules: false,
+      timings: false,
+      version: false,
+    },
   },
   watch: false,
   devtool: 'source-map',
-};
+}
 
-module.exports = config;
+module.exports = config
